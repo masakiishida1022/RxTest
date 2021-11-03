595420a5af53204aa81a8ec95b2cc3abe10742f3
masakiishida1022 <68501136+masakiishida1022@users.noreply.github.com>
2021-11-03 20:34:18 +0900

    実装完了

---
+++ RxTest/Observer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RxTest
+{
+    class Observer<T>
+    {
+        private Action<T> onNextAction;
+        public Observer(Action<T> action) => this.onNextAction = action;
+
+        public void onNext(T v) => this.onNextAction.Invoke(v);
+    }
+}
